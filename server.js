44b918e6654707152195cc321e4150d02c46deb6
waleedmalik7 <waleedmalik027@gmail.com>
2023-12-05 15:11:02 -0500

    added email feature, just need CSS now

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const nodemailer = require('nodemailer');
 
 const port = 8080;
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'automatedemailwaffleed@gmail.com',
+      pass: ''
+    }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -17,6 +28,21 @@ app.post('/email', (req,res) => {
         const { email, partner } = data[person];
         console.log(`Person: ${person}, Email: ${email}, Partner: ${partner}`);
     }
+
+    const mailOptions = {
+        from: 'automatedemailwaffleed@gmail.com',
+        to: 'waleedmalik027@gmail.com',
+        subject: 'Test Email',
+        text: 'This is a test email sent using Nodemailer.',
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.error('Error:', error);
+        }
+        console.log('Email sent:', info.response);
+    });
+
 });
 
 app.listen(port, () => {
