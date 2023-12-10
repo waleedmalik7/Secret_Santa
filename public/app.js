7b9f3e661e17fdbc2161fdb22fea73fe70cdff74
waleedmalik7 <waleedmalik027@gmail.com>
2023-12-10 05:46:44 -0500

    completed css, need to send response back from POST request (handle when email not sent). Look for how to host server for free

--- public/app.js
+++ public/app.js
@@ -1,66 +1,87 @@
 const name_count = document.querySelector("#names");
 const form_count = document.querySelector(".first-form");
 const title_container = document.querySelector(".title-container");
-const image = title_container.querySelector('img');
+const image = title_container.querySelector('#mainImage');
 const form_container_2 = document.querySelector(".form-container-2");
-const regex = /^([1-9]|[1-2]\d)$/; //TBD
+const backImage = document.querySelector('#backImage');
+
+const regex = /^([1-9]|[1-2]\d)$/; //regex for first input, we dont want too many names/emails
 
 let data = {};
 let names = [];
 
-form_count.addEventListener("submit",async (e)=>{
+form_count.addEventListener("submit", async (e)=>{
     e.preventDefault();
 
-    changePage();
+    
     
     const new_form = document.createElement("form");
+    changePage(new_form);
+
+    const input_container = document.createElement("div");
     const submit = document.createElement("input");
 
-    submit.setAttribute("type","submit");
-    submit.setAttribute("value","Submit");
+    input_container.setAttribute("class","input-container");
 
-    form_container_2.append(new_form);
+    submit.setAttribute("type","submit");
+    submit.setAttribute("value","Send Emails");
+
+    
     const number_of_names = Number(name_count.value);
 
     for(let i = 1; i < number_of_names + 1 ; i++){
-        console.log(i);
         const new_name = document.createElement("input");
         new_name.setAttribute("id",`name-${i}`);
-        new_name.setAttribute("placeholder", `name ${i}`);
+        new_name.setAttribute("placeholder", `Name ${i}`);
         new_name.setAttribute("type","text");
+        new_name.setAttribute("autocomplete","off");
+        new_name.setAttribute('required', 'true');
 
         const new_email = document.createElement("input");
         new_email.setAttribute("id",`email-${i}`);
-        new_email.setAttribute("placeholder", `email ${i}`);
+        new_email.setAttribute("placeholder", `Email ${i}`);
         new_email.setAttribute("type","text");
+        new_email.setAttribute("autocomplete","off");
+        new_email.setAttribute('required', 'true');
 
-        new_form.append(new_name);
-        new_form.append(new_email);
+        input_container.append(new_name);
+        input_container.append(new_email);
     }
+    new_form.append(input_container);
     new_form.append(submit);
+    form_container_2.append(new_form);
+    
 
     new_form.addEventListener("submit", (e)=> {
         e.preventDefault();
-        createData(new_form);
+        createData(input_container);
     });
 });
 
-const changePage = () =>{
+const changePage = (new_form) =>{
     name_count.style.display = 'none';
     form_count.style.display = 'none';
     image.style.width = '200px';
+    backImage.style.display = 'block';
+
+    backImage.addEventListener('click', ()=>{
+        name_count.style.display = 'block';
+        name_count.value = '';
+        form_count.style.display = 'block';
+        image.style.width = '400px';
+        backImage.style.display = 'none';
+        new_form.remove();
+    })
 };
-
-
 
 const createData = async (form)=>{
     const inputs = form.querySelectorAll("input");
-    const values = Array.from(inputs).slice(0,-1);
+    const values = Array.from(inputs);
     for(let i = 0; i < values.length; i += 2){
-        // data.push([values[i].value,values[i+1].value]);
         data[values[i].value] = {email: values[i+1].value};
         names.push(values[i].value);
     }
+    //randomize 3 times, because we want to ensure randomness
     console.log(names);
     randomizeData();
     console.log(names);
