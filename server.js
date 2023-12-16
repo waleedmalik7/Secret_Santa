20b6b9145e1f67016b19388860d4484856761563
waleedmalik7 <waleedmalik027@gmail.com>
2023-12-16 04:10:41 -0500

    added password

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const transporter = nodemailer.createTransport({
     secure: true,
     auth: {
       user: 'automatedemailwaffleed@gmail.com',
-      pass: ''
+      pass: 'wbplrludipfdpwgk'
     }
 });
 
